6fa292646c25a2122061dcef17de18ca1629274c
Christopher <chrissantista@gmail.com>
2017-08-01 20:51:17 -0300

    Aula 11 - Events

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -9,9 +9,30 @@ export class DataBindingComponent implements OnInit {
 
   url: string = "http://www.pudim.com.br/";
   urlImagem: string = 'https://mancilha.files.wordpress.com/2008/09/teste2.png';
+  palavra: string;
+  mouseSobre: boolean = false;
 
   getValor() {
     return 1;
+  }
+
+  alertaClick() {
+    alert("clicou no botão");
+  }
+
+  enterCampo(digitado) {
+    this.palavra = digitado;
+  }
+
+  alteraCampo(evento) {
+    console.log(
+      "Key = " + evento.key
+      + " valor do campo = " + evento.target.value
+    );
+  }
+
+  mouseSobreFora() {
+    this.mouseSobre = !this.mouseSobre;
   }
 
   constructor() { }
